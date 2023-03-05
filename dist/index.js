08acba89b669bf76425fe72fb8303031c9ad0ff9
Thanh Le <thanhlevan0203@gmail.com>
2023-03-05 15:06:03 -0800

    create mikroORM database

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const constants_1 = require("./constants");
 const core_1 = require("@mikro-orm/core");
 const main = async () => {
-    const orm = await core_1.MikroORM.init();
+    const orm = await core_1.MikroORM.init({
+        dbName: 'lireddit',
+        type: 'postgresql',
+        debug: !constants_1.__prod__
+    });
 };
 main();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+import { __prod__ } from './constants';
 import { MikroORM } from "@mikro-orm/core";
 
 const main = async () => {
-    const orm = await MikroORM.init();
-}
+    const orm = await MikroORM.init({
+        dbName: 'lireddit',
+        type: 'postgresql',
+        debug: !__prod__
+    });
+};
 
 main();
