ce27021e213f066deca7395a265cee52acb5e07a
Thanh Le <thanhlevan0203@gmail.com>
2023-03-05 14:59:01 -0800

    installed mikro-orm

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const main = async () => {
+    const orm = await core_1.MikroORM.init();
+};
+main();
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello")
+import { MikroORM } from "@mikro-orm/core";
+
+const main = async () => {
+    const orm = await MikroORM.init();
+}
+
+main();
