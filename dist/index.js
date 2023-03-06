3b8355eb96ce0d0d3fa3d52fdcec1a09c676694f
Thanh Le <thanhlevan0203@gmail.com>
2023-03-05 16:01:51 -0800

    create Post db entities

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const Post_1 = require("./entities/Post");
 const constants_1 = require("./constants");
 const core_1 = require("@mikro-orm/core");
 const main = async () => {
     const orm = await core_1.MikroORM.init({
+        entities: [Post_1.Post],
         dbName: 'lireddit',
         type: 'postgresql',
         debug: !constants_1.__prod__
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import { Post } from './entities/Post';
 import { __prod__ } from './constants';
 import { MikroORM } from "@mikro-orm/core";
 
 const main = async () => {
     const orm = await MikroORM.init({
+        entities: [Post],
         dbName: 'lireddit',
         type: 'postgresql',
         debug: !__prod__
