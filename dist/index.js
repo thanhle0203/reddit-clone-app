18751340d452161fad8bffb91694fe697630304b
Thanh Le <thanhlevan0203@gmail.com>
2023-03-07 23:01:50 -0800

    find Post

--- dist/index.js
+++ dist/index.js
@@ -9,8 +9,8 @@ const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
 const main = async () => {
     const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
     await orm.getMigrator().up();
-    const post = orm.em.create(Post_1.Post, { title: "my first post" });
-    await orm.em.persistAndFlush(post);
+    const posts = await orm.em.find(Post_1.Post, {});
+    console.log(posts);
 };
 main().catch((err) => {
     console.error(err);
