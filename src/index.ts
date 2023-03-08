f5e83cda2868c99a3acaec012d58afae1248c992
Thanh Le <thanhlevan0203@gmail.com>
2023-03-07 23:23:47 -0800

    create apolloServer and applyMiddleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import { HelloResolver } from './resolvers/hello';
 import { Post } from './entities/Post';
 import { __prod__ } from './constants';
 import { MikroORM } from "@mikro-orm/core";
 import microConfig from "./mikro-orm.config";
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
@@ -9,9 +13,23 @@ const main = async () => {
     // const post = orm.em.create(Post, {title: "my first post"});
     // await orm.em.persistAndFlush(post);
     //await orm.em.nativeInsert(Post, {title: 'my first post 2'});
-    const posts = await orm.em.find(Post, {});
-    console.log(posts);
+    // const posts = await orm.em.find(Post, {});
+    // console.log(posts);
+    const app = express();
+    
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    });
 
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(400, () => {
+        console.log('server started on localhost:4000')
+    })
+    
 };
 
 main().catch((err) => {
