a88707107aa9b5093e01fcf6e02a4ba3b737e97a
Thanh Le <thanhlevan0203@gmail.com>
2023-03-08 22:30:41 -0800

    create Post class

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -2,13 +2,14 @@ import { Post } from './../entities/Post';
 import { Ctx, Query, Resolver } from "type-graphql";
 import "core-js/features/reflect";
 import 'reflect-metadata';
+import { MyContext } from 'src/types';
 
 @Resolver()
 export class PostResolver {
     @Query(() => [Post])
     posts(
-        @Ctx() ctx: MyContext
-    ) {
-        return "hello world"
+        @Ctx() { em }: MyContext
+    ) : Promise<Post[]> {
+        return em.find(Post, {});
     }
 }
